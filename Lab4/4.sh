233a0610fd47030af3e569daa31706781ab1dc80
Ahana Agrawal <55917507+ahana2511@users.noreply.github.com>
2021-09-17 00:42:44 +0530

    OS COMMIT

---
+++ Lab4/4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Wordcount Script
+
+case $1 in
+	-linecount)
+            wc "$2" -l;;
+	-wordcount)
+			wc "$2" -w;;
+	-charcount)
+			wc "$2" -c;;
+	*);;
+esac
+© 2021 GitHub, Inc.
