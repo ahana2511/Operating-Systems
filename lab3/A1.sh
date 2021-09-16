36ecc6f73e9e18ca141426494f80269d62f95717
Ahana Agrawal <55917507+ahana2511@users.noreply.github.com>
2021-09-17 00:37:37 +0530

    OS COMMIT

---
+++ lab3/A1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Script to check for a prime number
+
+read input
+for((i=2;i<input;i++))
+do
+    if [ $[ $input % $i ] == 0 ]; then
+        echo "It is not a prime number"
+        exit 1;
+    fi
+done
+echo "It is a prime number"
+exit 0
+© 2021 GitHub, Inc.
